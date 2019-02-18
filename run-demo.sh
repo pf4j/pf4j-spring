d11dcde9a29af0ce7a9fdf3ef849595e9bab8ad9
decebals <decebal.suiu@gmail.com>
2019-02-19 01:19:08 +0200

    Fix run-demo scripts

--- run-demo.sh
+++ run-demo.sh
@@ -13,7 +13,8 @@ mkdir -p demo-dist/plugins
 
 # copy artifacts to demo-dist folder
 cp -r demo/app/target/pf4j-spring-demo-*/* demo-dist/
-cp demo/plugins/*/target/*plugin.jar demo-dist/plugins/
+cp demo/plugins/plugin1/target/pf4j-spring-demo-plugin1-*.zip demo-dist/plugins/
+cp demo/plugins/plugin2/target/pf4j-spring-demo-plugin2-*.zip demo-dist/plugins/
 cp demo/plugins/enabled.txt demo-dist/plugins/
 cp demo/plugins/disabled.txt demo-dist/plugins/
 
